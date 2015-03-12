08a04c41bf4834e3624bf2787cb557877ff6b1de
AtnesNess <atnes@bk.ru>
2015-03-12 23:19:35 +0500

    update

--- foolntp.py
+++ foolntp.py
@@ -7,6 +7,7 @@ NTP_HEADER_LENGTH = 48
 COUNT_TIME = 0.0016
 NTP_HEADER_FORMAT = ">BBBBII4sQQQQ"
 NTP_UTC_OFFSET = 2208988800
+
 TEMPLATE = (36, 2, 0, 238, 3602, 2077,
             b'\xc1\xbe\xe6A', 0, 0, 0, 0)
 
@@ -20,16 +21,18 @@ def main(shift):
 
     while True:
         data, addr = server.recvfrom(512)
+        receive_time = Decimal((2 ** 32)*(time.time()+NTP_UTC_OFFSET))
         res = struct.unpack(NTP_HEADER_FORMAT, data)
         tmp = TEMPLATE
         origin_time = res[-1]
-        receive_time = Decimal((2 ** 32)*(time.time()+NTP_UTC_OFFSET))
         trip_delay = receive_time - origin_time
-        send_time = Decimal((2 ** 32)*(time.time()+COUNT_TIME+NTP_UTC_OFFSET-shift))
         new_data = struct.pack(NTP_HEADER_FORMAT, tmp[0], tmp[1],
                                tmp[2], tmp[3], tmp[4],
-                               tmp[5], tmp[6], tmp[7], int(origin_time), int(receive_time), int(send_time-trip_delay))
+                               tmp[5], tmp[6], tmp[7], int(origin_time), int(receive_time),
+                               int(receive_time))
+
         server.sendto(new_data, addr)
+        print(trip_delay)
     server.close()
 
 if __name__ == "__main__":
